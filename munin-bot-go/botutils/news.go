78f92e4702d41dbf9d8a933f094222528d5c1c99
sundarakrishnang <sundarakrishnan.ganesh@gmail.com>
2021-09-05 18:21:17 +0200

    Removed useless files

---
+++ munin-bot-go/botutils/news.go
@@ -0,0 +1,42 @@
+package botutils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Content struct {
+	Articles []Article `json:"articles"`
+}
+type Article struct {
+	Title       string `json:"title"`
+	Description string `json:"description,omitempty"`
+	URL         string `json:"url"`
+}
+
+func GetContent(keyword string, API_KEY string) Content {
+	getURL := fmt.Sprintf("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=%s", API_KEY)
+	if keyword != "" {
+		getURL = fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&apiKey=%s", keyword, API_KEY)
+	}
+	resp, err := http.Get(getURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bytes, readErr := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	var c Content
+	errUnmarshal := json.Unmarshal(bytes, &c)
+	if errUnmarshal != nil {
+		log.Fatal(errUnmarshal)
+	}
+	return c
+}
---
+++ munin-bot-go/processor/newsprocessor.go
@@ -0,0 +1,20 @@
+package processor
+
+import (
+	"fmt"
+
+	"example.com/main/botutils"
+)
+
+func ProcessNews(keyword string, API_KEY string) string {
+	c := botutils.GetContent(keyword, API_KEY)
+	var reply string
+	if len(c.Articles) > 0 {
+		for i := 0; i < 3; i++ {
+			reply += fmt.Sprintf("Title: %+s\nURL: %+s\n", c.Articles[i].Title, c.Articles[i].URL)
+		}
+	} else {
+		reply += "I've searched far and wide to no avail.. :<"
+	}
+	return reply
+}
