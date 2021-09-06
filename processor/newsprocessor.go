7954292ac91c5477fce52e05cf34a917b0e822f9
Sundarakrishnan <56248899+sundarakrishnang@users.noreply.github.com>
2021-09-06 07:26:21 +0200

    Update newsprocessor.go

--- processor/newsprocessor.go
+++ processor/newsprocessor.go
@@ -9,6 +9,8 @@ import (
 func ProcessNews(keyword string, API_KEY string) string {
 	c := botutils.GetContent(keyword, API_KEY)
 	var reply string
+	log.Println(API_KEY)
+	log.Println(c.Articles)
 	if len(c.Articles) > 0 {
 		for i := 0; i < 3; i++ {
 			reply += fmt.Sprintf("Title: %+s\nURL: %+s\n", c.Articles[i].Title, c.Articles[i].URL)
