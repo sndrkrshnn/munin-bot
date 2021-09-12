6985a44cd24f48735a252d47c918292803531d26
sundarakrishnang <sundarakrishnan.ganesh@gmail.com>
2021-09-12 09:42:52 +0200

    bug fix v1.0

--- processor/imageprocessor.go
+++ processor/imageprocessor.go
@@ -9,8 +9,8 @@ import (
 func ProcessImage(keyword string, API_KEY string) string {
 	p := botutils.GetImageContent(keyword, API_KEY)
 	var reply string
-	if p.Photos.Total > 0 {
-		reply += fmt.Sprintf("https://live.staticflickr.com/%s/%s_%s_c.jpg", p.Photos.Photo[0].Server, p.Photos.Photo[0].ID, p.Photos.Photo[0].Secret)
+	if p.ImageContent.Photos.Total > 0 {
+		reply += fmt.Sprintf("https://live.staticflickr.com/%s/%s_%s_c.jpg", p.ImageContent.Photos.Photo[0].Server, p.ImageContent.Photos.Photo[0].ID, p.ImageContent.Photos.Photo[0].Secret)
 	} else {
 		reply += "I've searched far and wide to no avail.. My sources have restricted me for 24 hours as I bothered them too much :<\n"
 	}
